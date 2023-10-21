34762354d6441cba2d76e7615bb631a6d4c36015
RafaelCoelho1803 <128445225+RafaelCoelho1803@users.noreply.github.com>
2023-10-20 20:31:01 -0400

    Update Atividade1.py

--- Atividade1.py
+++ Atividade1.py
@@ -1,17 +1,72 @@
-MinhaLista = []
-while(True):
+Contagem = 0
+Cont1 = 0
+Cont2 = 0
+Cont3 = 0
+Cont4 = 0
+IdadeMaior = 0
+IdadeMenor = 100
+AvaliacaoMaiorIdade = ""  # Variável para armazenar a avaliação da maior idade
+AvaliacaoMenorIdade = ""  # Variável para armazenar a avaliação da menor idade
+Avaliacao = ""
+AvaliacaoM = ""
 
-    Geracao = input("Digite sua geração(X,Y,Z): ")
-    Sexo = input("Digite seu Sexo(F,M): ")
-    Futebol = input("Digite se já praticou Futebol(S,N): ")
-    Brasil = input("Digite se Brasil vai Ganha(S,N): ")
-    Continuar = input("Digite se voçê deseja continuar(S,N)")
+def verificar_idade_e_avaliacao(Idade, IdadeMaior, Qualidade):
+    if Idade > IdadeMaior:
+        return Idade, Qualidade
+    else:
+        return IdadeMaior, AvaliacaoMaiorIdade
 
-    MinhaLista.append([Geracao, Sexo, Futebol, Brasil])
+def verificar_idade_e_avaliacao_Menor(Idade, IdadeMenor, Qualidade):
+    if Idade < IdadeMenor:
+        return Idade, Qualidade
+    else:
+        return IdadeMenor, AvaliacaoMenorIdade
 
-    if Continuar == "N":
+while True:
+    Contagem = Contagem + 1
+    Idade = int(input("Qual a sua idade?"))
+    Qualidade = input("Qualidade do Atendimento 1-Ótimo, 2-Bom, 3-Regular, 4-Péssimo")
+    Refazer = input("Deseja refazer o formulário? (S , N)")
+
+    IdadeMaior, AvaliacaoMaiorIdade = verificar_idade_e_avaliacao(Idade, IdadeMaior, Qualidade)
+    IdadeMenor, AvaliacaoMenorIdade = verificar_idade_e_avaliacao_Menor(Idade, IdadeMenor, Qualidade)
+
+    if Contagem == 1:
+        Cont1 += 1
+    elif Contagem == 2:
+        Cont2 += 1
+    elif Contagem == 3:
+        Cont3 += 1
+    elif Contagem == 4:
+        Cont4 += 1
+
+    if Refazer == "N":
         break
 
-print(MinhaLista)
 
+
+if AvaliacaoMaiorIdade == "1":
+    Avaliacao = "Ótimo"
+elif AvaliacaoMaiorIdade == "2":
+    Avaliacao = "Bom"
+elif AvaliacaoMaiorIdade == "3":
+    Avaliacao = "Regular"
+elif AvaliacaoMaiorIdade == "4":
+    Avaliacao = "Ruim"
+
+if AvaliacaoMenorIdade == "1":
+    AvaliacaoM = "Ótimo"
+elif AvaliacaoMenorIdade == "2":
+    AvaliacaoM = "Bom"
+elif AvaliacaoMenorIdade == "3":
+    AvaliacaoM = "Regular"
+elif AvaliacaoMenorIdade == "4":
+    AvaliacaoM = "Ruim"
+
+
+
+print("O número de respostas ao formulário foi:", Contagem)
+print("Quantidade de avaliações foi de:", Cont1, "-Ótimo, ", Cont2, "-Bom, ", Cont3, "-Regular, ", Cont4, "-Ruim.")
+print("A maior idade registrada foi:", IdadeMaior,"e sua avialição foi: ", Avaliacao)
+print("A menor idade registrada foi:", IdadeMenor,"e sua avialição foi: ", AvaliacaoM)
 
